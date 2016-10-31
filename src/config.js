ab772dbdb3cbc6ebfd08dc62ece77fe25764c2ee
Manuel Serra <nitroin@hotmail.com>
2016-10-31 15:38:29 +0100

    Changed working path

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const LOG_LEVEL = process.env.LOG_LEVEL || "info";
-export const WORKING_PATH = process.env.NODE_ENV == "test" ? `${process.cwd()}/tmp1` : `${process.cwd()}/tmp2`;
+export const WORKING_PATH = process.env.NODE_ENV == "test" ? `${process.cwd()}/tmp` : `${process.cwd()}`;
 export const SRC_PATH = "/src";
 export const TEST_PATH = "/test";
 export const SERVICES_PATH = "/services";
